428c3b215983703b01812f2e405e5990e939a0a4
EdenLevy22 <103043688+EdenLevy22@users.noreply.github.com>
2022-06-16 12:46:30 +0300

    add static&template

---
+++ static/inin.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+
+  
+    const activePage = window.location.pathname;
+    console.log(activePage);
+    
+     document.querySelectorAll('a').forEach(link => {    
+      if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+      }
+    });
+    
+    }
+    
+    function showNames() {
+      document.getElementById("change").innerHTML = "LUCKY, SKY, ALPHA ";
+    }
+
+    function send() {
+      document.getElementById("send").innerHTML = "תודה רבה! ניצור עמך קשר ברגע שנמצא לך חבר מתאים";
+    }
